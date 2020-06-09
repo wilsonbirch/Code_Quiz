0978cfed4b2f6002482425a5e21915b5e132a3de
Wilson Birch <wilsonbirch@gmail.com>
2020-06-09 18:30:08 -0400

    got DOM updating working, pushing for input from Laith with what I have so far

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var startQuizEl = document.querySelector("#startQuiz");
 var counter = 60;
 var body = document.body;
 
+
 const myQuestions = ["Inside which HTML element do we put the JavaScript?", "How is a function in Javascript created?","Javascript is primarily used for?" ]
 const answers = new Array( );
 answers [0] = new Array ("<js>","<script>","<scr>","<java>");
@@ -38,8 +39,8 @@ function runQuiz(event){
                 return;
             }else{
                 $('#time').text(counter);
-              console.log("Timer --> " + counter);
-              counter.addEventListener("change", bodyTimer.textContent = ("Time left: "+counter))
+              //console.log("Timer --> " + counter);
+              counter.addEventListener("change", bodyTimer.textContent = ("Time left: "+counter));
             }
         }, 1000);
         
@@ -60,7 +61,7 @@ function runQuiz(event){
             cardBody.removeChild(cardText);
             cardBody.removeChild(startQuizEl);
 
-            console.log(myQuestions[i])
+            //console.log(myQuestions[i]);
             cardTitle.textContent = (myQuestions[i]);
             
             var answerContainer = document.createElement("container");
@@ -68,62 +69,57 @@ function runQuiz(event){
             answerContainer.setAttribute("class", "text-center");
 
             var answerRow1 = document.createElement("div");
-            var answer1 = document.createElement("div");
+            var answer1 = document.createElement("a");
             answerRow1.setAttribute ("class", "row");
             answerRow1.setAttribute ("style", "margin-top: 10px; margin-bottom: 10px");
             answerContainer.appendChild(answerRow1);
             answerRow1.appendChild(answer1);
 
             var answerRow2 = document.createElement("div");
-            var answer2 = document.createElement("div");
+            var answer2 = document.createElement("a");
             answerRow2.setAttribute ("class", "row");
             answerRow2.setAttribute ("style", "margin-top: 10px; margin-bottom: 10px");
             answerContainer.appendChild(answerRow2);
             answerRow2.appendChild(answer2);
 
             var answerRow3 = document.createElement("div");
-            var answer3 = document.createElement("div");
+            var answer3 = document.createElement("a");
             answerRow3.setAttribute ("class", "row");
             answerRow3.setAttribute ("style", "margin-top: 10px; margin-bottom: 10px");
             answerContainer.appendChild(answerRow3);
             answerRow3.appendChild(answer3);
 
             var answerRow4 = document.createElement("div");
-            var answer4 = document.createElement("div");
+            var answer4 = document.createElement("a");
             answerRow4.setAttribute ("class", "row");
             answerRow4.setAttribute ("style", "margin-top: 10px; margin-bottom: 10px");
             answerContainer.appendChild(answerRow4);
             answerRow4.appendChild(answer4);
 
             
+            answer1.setAttribute("id", "btn1");
+            answer2.setAttribute("id", "btn2");
+            answer3.setAttribute("id", "btn3");
+            answer4.setAttribute("id", "btn4");
             answer1.setAttribute("type", "button");
             answer2.setAttribute("type", "button");
             answer3.setAttribute("type", "button");
             answer4.setAttribute("type", "button");
-            answer1.setAttribute("class", "btn btn-outline-dark btn-sm ");
-            answer2.setAttribute("class", "btn btn-outline-dark btn-sm ");
-            answer3.setAttribute("class", "btn btn-outline-dark btn-sm ");
-            answer4.setAttribute("class", "btn btn-outline-dark btn-sm ");
+            answer1.setAttribute("class", "btn btn-outline-dark btn-sm btn1");
+            answer2.setAttribute("class", "btn btn-outline-dark btn-sm btn2");
+            answer3.setAttribute("class", "btn btn-outline-dark btn-sm btn3");
+            answer4.setAttribute("class", "btn btn-outline-dark btn-sm btn4");
             answer1.textContent = answers[i][0];
             answer2.textContent = answers[i][1];
             answer3.textContent = answers[i][2];
             answer4.textContent = answers[i][3];
 
-
-            //answer1 = 
-
-
-            
-            
             
         }
 
-
-
     } 
-
-        
-    
+         
 }
 
 startQuizEl.addEventListener("click", runQuiz);
+
