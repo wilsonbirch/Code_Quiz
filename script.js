8e1e24a682656186dd67368f4eb4f06474876add
Wilson Birch <wilsonbirch@gmail.com>
2020-06-09 13:25:39 -0400

    created CSS timer for styling, initialized if loop to begin timer

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// create all variables needed for the script including start quiz elements, count, and element creators
 var startQuizEl = document.querySelector("#startQuiz");
+var count = 60;
+var body = document.body;
+
 
 function runQuiz(event){
-    alert ("Hello World!")
+
+    if (count>0){
+
+        //create timer div once timer begins
+        var bodyTimer = document.createElement("div");
+        body.appendChild(bodyTimer);
+        bodyTimer.setAttribute("class", "card text-white text-center bg-dark mb-3");
+        bodyTimer.setAttribute("style", "max-width: 30px; min-width: 20px; margin-left: 50%; margin-top: 20px");
+        bodyTimer.textContent = count;
+
+    }
+    
 }
 
 startQuizEl.addEventListener("click", runQuiz);
